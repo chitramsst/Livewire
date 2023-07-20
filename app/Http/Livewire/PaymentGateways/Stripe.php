6a5dfb11087a9ef12e3a00d451d6003f6154ec8d
Chitra <chitra@Chitras-MacBook-Pro.local>
2023-07-20 12:48:12 +0530

    stripe

--- app/Http/Livewire/PaymentGateways/Stripe.php
+++ app/Http/Livewire/PaymentGateways/Stripe.php
@@ -3,11 +3,110 @@
 namespace App\Http\Livewire\PaymentGateways;
 
 use Livewire\Component;
+use Stripe as StripeComponent;
 
 class Stripe extends Component
 {
-    public function render()
-    {
-        return view('livewire.payment-gateways.stripe');
+  public $token;
+  public function render()
+  {
+    return view('livewire.payment-gateways.stripe');
+  }
+  /* stripe payment section (after transaction) */
+  public function stripePost()
+  {
+
+    $stripe = new \Stripe\StripeClient('sk_test_51NThAcSEMAXsEqdS1m8Av7JZVyDLgoYiaOskSeiOQeccmAjFR0HSFUGjIpuanVQLR4pTU0KvpcQO0ZNZgwqiW1ry00EwyEW8Vt');
+
+    $status = $stripe->paymentIntents->create([
+      'amount' => 500,
+      'currency' => 'INR',
+      'payment_method' => 'pm_card_visa',
+    ]);
+
+    dd($status);
+    return redirect('success');
+
+
+    // $settings = new MasterSetting();
+    // $site = $settings->siteData();
+    $site_currency_code = 'INR';
+    //  $id = $request->id;
+    //  $email = $request->email;
+    //  $settings=new MasterSetting();
+    //  $site=$settings->siteData();
+    // $stripe = new \StripeComponent\StripeClient('sk_test_51NThAcSEMAXsEqdS1m8Av7JZVyDLgoYiaOskSeiOQeccmAjFR0HSFUGjIpuanVQLR4pTU0KvpcQO0ZNZgwqiW1ry00EwyEW8Vt');
+
+
+    // StripeComponent\Stripe::setApiKey("sk_test_51NThAcSEMAXsEqdS1m8Av7JZVyDLgoYiaOskSeiOQeccmAjFR0HSFUGjIpuanVQLR4pTU0KvpcQO0ZNZgwqiW1ry00EwyEW8Vt");
+
+    // // try {
+    //       $charge =   StripeComponent\Charge::create ([
+    //         "amount" => 50000,
+    //         "currency" => "USD",
+    //         //"source" => $this->token,
+    //         "source" => "tok_mastercard",
+    //         "description" => "customer payment"
+
+    //     ]);
+    //  $customer = (Session::has('customer')) ? Session::get('customer') : "";
+
+    // if ($charge->status == "succeeded") {
+      /* if the payment is success */
+      $amount = 50000;
+      /* stripe payment hist ory updation */
+      //   $payment_history = new CustomerPaymentHistory();
+      //   $payment_history->invoice_number=generate_customerinvoicenumber();
+      //   $payment_history->invoice_id=$request->id;
+      //   $payment_history->payment_id=$charge->id;
+      //   $payment_history->email=$request->email;
+      //   $payment_history->date=\Carbon\Carbon::today()->toDateString();
+      //   $payment_history->amount_paid= $request->amount;
+      //   $payment_history->invoice_amount= $request->amount;
+      //   $payment_history->payment_status=$charge->status;
+      //   $payment_history->save();
+      /*get customer details */
+      //    $invoice = Invoice::find($request->id);
+      //   /*payment updation*/
+      //   $payment = new Payment();
+      //   $payment->customer_id =$invoice->customer_id;
+      //   $payment->paid_date = \Carbon\Carbon::today()->toDateString();
+      //   $payment->paid =  $request->amount;
+      //   $payment->payment_mode = 'stripe';
+      //   $payment->invoice_number =  $invoice->invoice_number;
+      //   $payment->payment_number = generate_paymentnumber();
+      //   $payment->note = "Payment done by customer";
+      //   $payment->save();
+
+      /* update payment amount to invoice table */
+      //  $invoice->invoice_status = ($invoice->total - ($invoice->paid + $request->amount) == 0) ? '1' : '0';
+      //  $invoice->paid = $invoice->paid + $request->amount;
+      //  $invoice->save();
+      //  if(!empty($customer)) {
+      //    return redirect('/customer/invoices')->with('message', 'Payment Sent Successfully.');
+      //  } else {
+      //    return view('transaction-success');
+      //  }
+      // } else
+      // {
+      //   /* if transaction is not successful */
+      //   if(!empty($customer)) {
+      //     return redirect('/customer/invoices')->with('error', 'Something went wrong. Please contact support team.');
+      //   } else {
+      //     return view('transaction-failure');
+      //   }
+      // }
     }
+
+    // } catch ( \Exception $e ) {
+    //     dd($e);
+    //       /* if transaction has any issues */
+    //     //   if(!empty($customer)) {
+    //     //           return redirect('/customer/invoices')->with('error', 'Something went wrong. Please contact support team.');
+    //     //   } else {
+    //     //           return view('transaction-failure');
+    //     //   }
+    //  }
+
+  // }
 }
--- resources/views/livewire/payment-gateways/stripe.blade.php
+++ resources/views/livewire/payment-gateways/stripe.blade.php
@@ -7,71 +7,191 @@ publishable key: pk_test_51NThAcSEMAXsEqdSE8BfHTtUoBzEH0cHOfDk9zsfElRgbtHfRg3jrP
 secret key: sk_test_51NThAcSEMAXsEqdS1m8Av7JZVyDLgoYiaOskSeiOQeccmAjFR0HSFUGjIpuanVQLR4pTU0KvpcQO0ZNZgwqiW1ry00EwyEW8Vt -->
 
 
-<div class="container">         
-         <div class="row">
-          <h3 style="text-align: center;margin-top: 40px;margin-bottom: 40px;">Laravel 9 Stripe Payment Gateway Integration - Techsolutionstuff</h3>
-            <div class="col-md-6 col-md-offset-3">
-               <div class="panel panel-default credit-card-box">
-                  <div class="panel-heading" >
-                     <div class="row">
-                        <h3>Payment Details</h3>
-                        <div>                            
-                           <img class="img-responsive pull-right" src="http://i76.imgup.net/accepted_c22e0.png">
-                        </div>
-                     </div>
-                  </div>
-                  <div class="panel-body">
-                     @if (Session::has('success'))
-                     <div class="alert alert-success text-center">
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{ __('all.stripe_payment_gateway')}}</title>
+    <!-- <link rel="stylesheet" href="{{ asset('assets/common/bootstrap.min.css') }}" />
+    <script src="{{asset('assets/backend/plugins/jquery/jquery.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('assets/install/font-awesome/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/install/css/backend-plugin.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/install/css/backende209.css?v=1.0.0')}}"> -->
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+
+
+    <style type="text/css">
+        .panel-title {
+            display: inline;
+            font-weight: bold;
+        }
+        .display-table {
+            display: table;
+        }
+        .display-tr {
+            display: table-row;
+        }
+        .display-td {
+            display: table-cell;
+            vertical-align: middle;
+            width: 61%;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <br><br><br><br>
+    <br><br><br><br>
+    <div class="card card-warning">
+        <div class="card-header">
+          <h3 class="card-title">{{ __('all.payment_details')}}</h3>
+        </div>
+        <div class="card-body">
+            <div class="panel-body">
+                @if (Session::has('success'))
+                    <div class="alert alert-success text-center">
                         <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
-                        <p>{{ Session::get('success') }}</p><br>
-                     </div>
-                     @endif
-                     <br>
-                     <form role="form"  method="post" class="require-validation" data-cc-on-file="false" data-stripe-publishable-key="{{ env('STRIPE_KEY') }}" id="payment-form">
-                        @csrf
-                        <div class='form-row row'>
-                           <div class='col-xs-12 col-md-6 form-group required'>
-                              <label class='control-label'>Name on Card</label> 
-                              <input class='form-control' size='4' type='text'>
-                           </div>
-                           <div class='col-xs-12 col-md-6 form-group required'>
-                              <label class='control-label'>Card Number</label> 
-                              <input autocomplete='off' class='form-control card-number' size='20' type='text'>
-                           </div>                           
-                        </div>                        
-                        <div class='form-row row'>
-                           <div class='col-xs-12 col-md-4 form-group cvc required'>
-                              <label class='control-label'>CVC</label> 
-                              <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
-                           </div>
-                           <div class='col-xs-12 col-md-4 form-group expiration required'>
-                              <label class='control-label'>Expiration Month</label> 
-                              <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
-                           </div>
-                           <div class='col-xs-12 col-md-4 form-group expiration required'>
-                              <label class='control-label'>Expiration Year</label> 
-                              <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
-                           </div>
+                        <p>{{ Session::get('success') }}</p>
+                    </div>
+                @endif
+                <form class="require-validation"
+                      data-cc-on-file="false"
+                      data-stripe-publishable-key="pk_test_51NThAcSEMAXsEqdSE8BfHTtUoBzEH0cHOfDk9zsfElRgbtHfRg3jrPxAsewbDYf996UkMaiPFgYMaQpfoQKt5cqY00tYjC1aN8"
+                      id="payment-form">
+                    @csrf
+                    <div class='form-group'>
+                        <div class='col-xs-12 form-group required'>
+                            <label class='control-label'>{{ __('all.name_on_card')}}</label> <input
+                                class='form-control' size='4' type='text'>
                         </div>
-                      {{-- <div class='form-row row'>
-                         <div class='col-md-12 error form-group hide'>
-                            <div class='alert-danger alert'>Please correct the errors and try
-                               again.
-                            </div>
-                         </div>
-                      </div> --}}
-                        <div class="form-row row">
-                           <div class="col-xs-12">
-                              <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now</button>
-                           </div>
+                    </div>
+                    <div class='form-group'>
+                        <div class='col-xs-12 form-group required'>
+                            <label class='control-label'>{{ __('all.card_number')}}</label> <input
+                                autocomplete='off' class="form-control card-number" size="16" id="number" maxlength="19"
+                                type='text'>
                         </div>
-                     </form>
-                  </div>
-               </div>
+                    </div>
+                    <div class='form-group'>
+                        <div class='col-xs-12 col-md-4 form-group cvc required'>
+                            <label class='control-label'>{{ __('all.cvc')}}</label> <input autocomplete='off'
+                                                                            class='form-control card-cvc' placeholder='ex. 311' maxlength='4'
+                                                                            type='text'>
+                        </div>
+                        <div class='col-xs-12 col-md-4 form-group expiration required'>
+                            <label class='control-label'>{{ __('all.expiration_month')}}</label> <input
+                                class='form-control card-expiry-month' placeholder='MM' size='2' maxlength="2"
+                                type='text'>
+                        </div>
+                        <div class='col-xs-12 col-md-4 form-group expiration required'>
+                            <label class='control-label'>{{ __('all.expiration_year')}}</label> <input
+                                class='form-control card-expiry-year' placeholder='YYYY' size='4' maxlength='4'
+                                type='text'>
+                        </div>
+                    </div>
+                    <div class='form-row row'>
+                        <div class='col-md-12 error form-group hide'>
+                            <div class='alert-danger alert'>{{ __('all.error_correction')}}</div>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <button class="btn btn-warning btn-lg btn-block" wire:click="stripePost">{{ __('all.pay_now')}} 100 </button>
+                        </div>
+                    </div>
+                </form>
             </div>
-         </div>
-         @push('js')
-         <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
-         @endpush
+
+      </div>
+
+    </div>
 </div>
+</body>
+
+<script src="https://js.stripe.com/v2/"></script>
+<script src="https://js.stripe.com/v3/"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+@push('js')
+<script type="text/javascript">
+    $(function() {
+        'use strict';
+        // $.ajaxSetup({
+        //     headers: {
+        //         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        //     }
+        // });
+        var $form         = $(".require-validation");
+        $('form.require-validation').bind('submit', function(e) {
+            var $form         = $(".require-validation"),
+                inputSelector = ['input[type=email]', 'input[type=password]',
+                    'input[type=text]', 'input[type=file]','input[type=hidden]',
+                    'textarea'].join(', '),
+                $inputs       = $form.find('.required').find(inputSelector),
+                $errorMessage = $form.find('div.error'),
+                valid         = true;
+            $errorMessage.addClass('hide');
+            $('.has-error').removeClass('has-error');
+            $inputs.each(function(i, el) {
+                var $input = $(el);
+                if ($input.val() === '') {
+                    /* if the input value is empty */
+                    $input.parent().addClass('has-error');
+                    $errorMessage.removeClass('hide');
+                    e.preventDefault();
+                }
+            });
+            if (!$form.data('cc-on-file')) {
+                /* stripe card creation */
+                e.preventDefault();
+                Stripe.setPublishableKey($form.data('stripe-publishable-key'));
+                Stripe.card.createToken({
+                    number: $('.card-number').val(),
+                    cvc: $('.card-cvc').val(),
+                    exp_month: $('.card-expiry-month').val(),
+                    exp_year: $('.card-expiry-year').val(),
+                    address_zip: $('.address_zip').val(),
+                }, stripeResponseHandler);
+            }
+        });
+        function stripeResponseHandler(status, response) {
+            //alert(JSON.stringify(response));
+            'use strict';
+            if (response.error) {
+             /* if the response has error */
+                $('.error')
+                    .removeClass('hide')
+                    .find('.alert')
+                    .text(response.error.message);
+            } else {
+                /* if the response has success */
+                var token = response['id'];
+                alert(token);
+                @this.set('token',token);
+                // $form.find('input[type=text]').empty();
+                // $form.append("<input type='hidden' name='amount' value='" + $('#amount').val() + "'/>");
+                // $form.append("<input type='hidden' name='email' value='" + $('#email').val() + "'/>");
+                // $form.append("<input type='hidden' name='id' value='" + $('#id').val() + "'/>");
+                $form.append("<input type='hidden' name='stripeToken' value='" + token + "'/>");
+                $form.get(0).submit();
+            }
+        }
+    });
+</script>
+
+<script type="text/javascript">
+  /* enable spacing for credit card number     */
+  $('#number').on('keyup', function(e){
+    var val = $(this).val();
+    var newval = '';
+    val = val.replace(/\s/g, '');
+    for(var i = 0; i < val.length; i++) {
+      if(i%4 == 0 && i > 0) newval = newval.concat(' ');
+      newval = newval.concat(val[i]);
+    }
+    $(this).val(newval);
+   });
+</script>
+@endpush
+
+</html>
